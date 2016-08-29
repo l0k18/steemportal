a5c675e468288320b8807dae2989d82342918167
l0k1 <l0k1@null.net>
2016-08-29 16:10:45 +0300

    Completed stubs for interface modules

---
+++ src/web.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+#
+# web.py
+#
+# Interface frontend for a webserver interface module
+#
+# Here's your stinkin' licence: http://unlicense.org/
+#
+# - l0k1
+#
+# Contact details for l0k1
+# steemit.com - https://steemit.com/@l0k1
+# bitmessage - BM-2cXWxTVaXJbNyMxv5tAjNg87xS98hrAg8P
+# torchat - xq6xcvqc2vy34qtx
+# email - l0k1@null.net
+
+def printmodulename ():
+    print ("Importing web frontend module")
