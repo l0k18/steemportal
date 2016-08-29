ac4d26467cabaf94bd7ec88cd1a6c74403b78aab
l0k1 <l0k1@null.net>
2016-08-29 15:15:35 +0300

    Redesign to enable multiple frontends

---
+++ src/main.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+#
+# main.py
+#
+# central launcher that processes commandline arguments and selects interface.
+#
+# Here's your stinkin' licence: http://unlicense.org/
+#
+# - l0k1
+#
+# Contact details for l0k1
+# steemit.com - https://steemit.com/@l0k1
+# bitmessage - BM-2cXWxTVaXJbNyMxv5tAjNg87xS98hrAg8P
+# torchat - xq6xcvqc2vy34qtx
+# email - l0k1@null.net
+
+import sys, os, argparse
+
+parser = argparse.ArgumentParser(description="Steem Blockchain Interface")
+parser.add_argument ('frontend', metavar='FRONTEND', type=str, nargs="?", default="gtk3", help="Interface frontend library to use, defaults to gtk3")
+args = parser.parse_args()
+
