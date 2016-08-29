a31a5999e2b17e752cbbabfaeb40503028821885
l0k1 <l0k1@null.net>
2016-08-29 16:32:23 +0300

    added comments to gschema install/compiler

--- install_compile_schema.sh
+++ install_compile_schema.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# This file currently is not in use, but will be implemented as the gtk3/glib2 parts
+# are added
 sudo cp steemportal.gschema.xml /usr/share/glib-2.0/schemas/
 sudo glib-compile-schemas /usr/share/glib-2.0/schemas
